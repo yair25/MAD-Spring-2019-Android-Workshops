8991b95a361315e8df8f0acc2f93c12cfdac4207
Yair Nieto <yair.nieto.cofo2015@gmail.com>
2019-01-16 17:58:03 -0600

    Lesson 2, Instructions in README.txt & Comments

--- MAD Implicit/app/src/main/java/com/yairn/mad/madimplicit/MainActivity.java
+++ MAD Implicit/app/src/main/java/com/yairn/mad/madimplicit/MainActivity.java
@@ -8,26 +8,59 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+     * onCreate is an Android method that is called when the activity is first created.
+     * This is where you should do all of your normal static set up: create views, bind data to lists, etc.
+     * This method also provides you with a Bundle containing the activity's previously frozen state, if there was one.
+     * Always followed by onStart().
+     *
+     * In this method, the contentView, or in other words the UI layout named activity_main
+     * will be associated with this code, and it will create/display it.
+     * This is where listeners can bounded to most UIs, or just use the OnClick
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Bind code and Layout/UI
         setContentView(R.layout.activity_main);
     }
 
+    /*
+     * If the Button View's onClick attribute is set, it is required to make the exact named method.
+     * It must take a View parameter.
+     * This is where the all the logic for clicking the button goes.
+     *
+     * In the method onClickMaps, an Intent is made to start the Maps app.
+     * If the device does not have a Maps app, a message (Toast) will be displayed.
+     */
     public void onClickMaps(View view) {
         Intent intent = new Intent();
+        // Action is set to main, so that the app is the main focus
         intent.setAction(Intent.ACTION_MAIN);
+        // Category is set to CATEGORY_APP_MAPS, so any app that is considered a map can be opened
         intent.addCategory(Intent.CATEGORY_APP_MAPS);
+        // Flag is set to FLAG_ACTIVITY_NEW_TASK, because a whole new task (app) will start
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
+        // Will try to start the activity
         try {
             startActivity(intent);
         } catch (Exception e) {
+            // Will ensure that the app does not crash if there is no Maps app found
             Toast.makeText(this, "No Maps App Found.", Toast.LENGTH_LONG).show();
         }
 
     }
 
+    /*
+     * If the Button View's onClick attribute is set, it is required to make the exact named method.
+     * It must take a View parameter.
+     * This is where the all the logic for clicking the button goes.
+     *
+     * In the method onClickBrowser, an Intent is made to start the Browser app.
+     * If the device does not have a Browser app, a message (Toast) will be displayed.
+     */
     public void onClickBrowser(View view) {
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_MAIN);
@@ -37,10 +70,19 @@ public class MainActivity extends AppCompatActivity {
         try {
             startActivity(intent);
         } catch (Exception e) {
+            // Will ensure that the app does not crash if there is no Maps app found
             Toast.makeText(this, "No Browser App Found.", Toast.LENGTH_LONG).show();
         }
     }
 
+    /*
+     * If the Button View's onClick attribute is set, it is required to make the exact named method.
+     * It must take a View parameter.
+     * This is where the all the logic for clicking the button goes.
+     *
+     * In the method onClickMusic, an Intent is made to start the Music app.
+     * If the device does not have a Music app, a message (Toast) will be displayed.
+     */
     public void onClickMusic(View view) {
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_MAIN);
@@ -54,6 +96,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*
+     * If the Button View's onClick attribute is set, it is required to make the exact named method.
+     * It must take a View parameter.
+     * This is where the all the logic for clicking the button goes.
+     *
+     * In the method onClickCalc, an Intent is made to start the Calculator app.
+     * If the device does not have a Calculator app, a message (Toast) will be displayed.
+     */
     public void onClickCalc(View view) {
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_MAIN);
---
+++ MAD Implicit/app/src/main/java/com/yairn/mad/madimplicit/README.txt
@@ -0,0 +1,11 @@
+These are some instructions on how to make this application.
+There are also some hints/suggestions on what to say to help the understanding.
+
+Steps:
+1. Start with app > res > layout > activity_main.xml
+2. With ConstraintLayout, add Buttons with attributes (Optional: Add onClick attributes, but recommended)
+3. Explain what app > res > values > strings is used for and rest of the xmls in the folder
+4. Add appropriate string to strings.xml
+5. Next go to app > java > com.________ > MainActivity
+6. Code and link to UI
+
