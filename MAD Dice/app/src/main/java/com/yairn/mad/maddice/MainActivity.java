eea60ba25a649c5a7a7a32683cf7bb45d41b17a9
Yair Nieto <yair.nieto.cofo2015@gmail.com>
2019-01-16 17:29:30 -0600

    Lesson 1, Instructions in README.txt & Comments

--- MAD Dice/app/src/main/java/com/yairn/mad/maddice/MainActivity.java
+++ MAD Dice/app/src/main/java/com/yairn/mad/maddice/MainActivity.java
@@ -11,16 +11,33 @@ public class MainActivity extends AppCompatActivity {
     private ImageView diceImageView;
 //    private Button rollButton;
 
+    // Array that stores the Images/drawables
     private final int[] diceImages = new int[] {
             R.drawable.dice_one, R.drawable.dice_two, R.drawable.dice_three, R.drawable.dice_four,
             R.drawable.dice_five, R.drawable.dice_six};
 
+    /*
+    * onCreate is an Android method that is called when the activity is first created.
+    * This is where you should do all of your normal static set up: create views, bind data to lists, etc.
+    * This method also provides you with a Bundle containing the activity's previously frozen state, if there was one.
+    * Always followed by onStart().
+    *
+    * In this method, the contentView, or in other words the UI layout named activity_main
+    * will be associated with this code, and it will create/display it.
+    * Additionally, this is where the code is being bounded to the XML/UI, by using findViewByID.
+    * This is where listeners can bounded to most UIs, or just use the OnClick
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Bind of code and Layout/UI
         setContentView(R.layout.activity_main);
 
+        // Linking/Bounding UI to the code
         diceImageView = findViewById(R.id.DiceImageView);
+
+//         // Optional, but we can also not use onClick and set up a listener for the button
 //        rollButton = findViewById(R.id.RollButton);
 //
 //        rollButton.setOnClickListener(new View.OnClickListener() {
@@ -35,11 +52,23 @@ public class MainActivity extends AppCompatActivity {
 //        });
     }
 
+    /*
+    * If the Button View's onClick attribute is set, it is required to make the exact named method.
+    * It must take a View parameter.
+    * This is where the all the logic for clicking the button goes.
+    *
+    * In the method onClickRollButton, the roll of a dice is being simulated by getting a random integer,
+    * and setting the image resource based on the result. Finally a toast (message) is shown on the screen.
+    */
     public void onClickRollButton(View view) {
         // Returns a double value with a positive sign, greater than or equal to 0.0 and less than 1.0
         int rndImage = (int) (Math.random() * 6);
+
+        // set the image resource to the Image View
         diceImageView.setImageResource(diceImages[rndImage]);
         rndImage++;
-        Toast.makeText(this, "You Rolled : " + rndImage, Toast.LENGTH_LONG).show();
+
+        // Will show the message ""You Rolled: rndImage"
+        Toast.makeText(this, "You Rolled: " + rndImage, Toast.LENGTH_LONG).show();
     }
 }
---
+++ MAD Dice/app/src/main/java/com/yairn/mad/maddice/README.txt
@@ -0,0 +1,11 @@
+These are some instructions on how to make this application.
+There are also some hints/suggestions on what to say to help the understanding.
+
+Steps:
+1. Start with app > res > layout > activity_main.xml
+2. With ConstraintLayout, add an ImageView and a Button
+3. Explain most of the attributes for both ImageView and Button, as well as what is ConstraintLayout
+4. Add attributes while explaining
+5. Next download the dice images from the link (https://drive.google.com/open?id=1B30PH2kByVIHKfzK1BaD_gm3FmQdxnV7), and save them to app > res > drawable
+6. Next go to app > java > com.________ > MainActivity
+7. Code and link to UI
